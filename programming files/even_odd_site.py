764067dba15f7fc5ce81e7b11d27106e39815e7f
Colin <35661747+colinfrankel@users.noreply.github.com>
2026-04-07 16:51:16 -0400

    Generate HTML table for even and odd numbers
    Added HTML structure and logic to categorize numbers into even and odd in the generated table.

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,16 @@
+# Grab numbers from html file and add header -CF
 with open("numbers.html", "w") as f:
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    # Loop 50 times and create html table sorting numbers into even and odd categories -CF
     for i in range(1, 50):
         if i % 2 == 0:
             g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         if i % 3 == 0:
             g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+    # Close tags for table -CF
     f.write("</table>\n</body>\n</html>")
-
+# Write back to original file
 with open("numbers.html"") as f:
     print(f.read())
     
