09896f75fa01d4e15f6b1f4bf96fa27475b4166a
JD Yergeau <Joseph.Yergeau@uvm.edu>
2026-04-07 16:49:55 -0400

    Enhance code readability with comments
    Added comments to clarify the Fibonacci sequence logic and sum calculation.

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,15 +1,25 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
+
+        // Define the first two numbres in the fibonacci sequence
         int a = 0, b = 1;
         int sumEven = 0;
+
+        // Keep summing until the sum is 4,000,000 or higher
         while (b < 4000000) {
+
+            // If the current fib is even, add it to the sum
             if (b % 2 == 0) {
                 sumEven += b;
             }
+
+            // Update the values of a and b
             int temp = b;
             b = a + b;
             a = temp;
         }
+
+        // Print out the sum
         System.out.println(sumEven);
     }
 }
