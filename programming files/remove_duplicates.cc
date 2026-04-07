73a62da917d004e1c59b8c974970ff8c45b811d3
sam <smatunas@uvm.edu>
2026-04-07 17:32:04 -0400

    bruh i fixed it ing

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -5,15 +5,16 @@ using namespace std;
 
 vector<int> remove_duplicates(vector<int> arr, vector<int> &removed_elements) {
   vector<int> unique_arr;
+  sort(arr.begin(), arr.end());
   int n = arr.size();
-  for (int i = 1; i < n; i--) {
-    if (i == 0 || arr[i] != arr[i + 1]) {
+  for (int i = 0; i < n; i++) {
+    if (i == 0 || arr[i] != arr[i - 1]) {
       unique_arr.push_back(arr[i]);
     } else {
       removed_elements.push_back(arr[i]);
     }
   }
-  sort(arr.begin(), arr.end());
+
   return unique_arr;
 }
 
