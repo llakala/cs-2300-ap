d77b4efc60e25f366d531c29e2a319b5f03b5d4e
JD Yergeau <Joseph.Yergeau@GMail.com>
2026-04-07 17:06:08 -0400

    Bugs Introduced

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,11 +1,12 @@
 #returns the sum of even numbers in a fibonacci sequence
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
+if b < 4000000:
     if b % 2 == 0:
-        sum_even += b
-    a, b = b, a+b
+        sum_even = a + b
+    a, b = b, a
 
 print(sum_even)
 
 # answer should be 4613732
+# JDY
