7f8c5281e3e3fdc5e4c95536a56b96430f32ad8d
wenaf <wnorthfletcher@gmail.com>
2026-04-07 17:21:37 -0400

    compiler is broken on ARM64

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -2,24 +2,26 @@
 #include <fstream>
 using namespace std;
 
-// // Received clang error:
-// int main() {
-//     ofstream file;
-//     file.open("numbers.html");
-//     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
-//     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
-//     for (int i = 1; i <= 50; i++) {
-//         if (i % 2 == 0) {
-//             file << "<tr><td>" << i << "</td><td></td></tr>\n";
-//         }
-//         else {
-//             file << "<tr><td></td><td>" << i << "</td></tr>\n";
-//         }
-//     }
-//     file << "</table>\n</body>\n</html>";
-//     file.close();
-//     ifstream input("numbers.html");
-//     cout << input.rdbuf();
-//     input.close();
-//     return 0;
-// }
+// Received clang error: linker command failed with exit code 1 
+// ld: symbol(s) not found for architecture arm64
+// WNFLETCH
+int main() {
+    ofstream file;
+    file.open("numbers.html");
+    file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
+    file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    for (int i = 1; i <= 50; i++) {
+        if (i % 2 == 0) {
+            file << "<tr><td>" << i << "</td><td></td></tr>\n";
+        }
+        else {
+            file << "<tr><td></td><td>" << i << "</td></tr>\n";
+        }
+    }
+    file << "</table>\n</body>\n</html>";
+    file.close();
+    ifstream input("numbers.html");
+    cout << input.rdbuf();
+    input.close();
+    return 0;
+}
