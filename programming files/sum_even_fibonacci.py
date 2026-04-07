d3d81918c3f2c953a005f154e3fe0adfa782c95a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-07 16:51:11 -0400

    add comments for sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,5 +1,10 @@
+# Initial terms of fibonacci sequence (1,1 would also work)
 a, b = 0, 1
+
+# accumulator for the sum of all even values in the fibonacci sequence
 sum_even = 0
+
+# keep going until the term in the fibonacci sequence is >= 4 million
 while b < 4000000:
     if b % 2 == 0:
         sum_even += b
