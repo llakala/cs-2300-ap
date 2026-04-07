01d843d028bc24d894614155de5a102431f072e8
kati <wkf@amhran.net>
2026-04-07 17:35:36 -0400

    fixd it

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -7,13 +7,13 @@ int main()
 {
     std::string date_str = "2022-03-17 10:45:30";
     std::tm date_obj = {};
-    std::istringstream ss(date_obj);
-    ss >> std::get_time(&date_str, "%Y-%m-%d %H:%M:%S");
+    std::istringstream ss(date_str);
+    ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss >> std::put_time(&date_obj, "%m/%d/%Y %H:%S")
+    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%S");
     std::string formatted_date = formatted_date_ss.str();
 
-    cout << formatted_date << std::endl;
+    std::cout << formatted_date << std::endl;
 
     return 0;
 }
